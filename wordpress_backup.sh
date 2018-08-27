178ef3b67acc81e58e4abf36e34be260a66c3be2
Sellitus <sellitus@gmail.com>
2018-08-26 21:17:52 -0700

    Updated wordpress_backup.sh with the latest working changes.

--- wordpress_backup.sh
+++ wordpress_backup.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# NOTE: You will need to create a /root/.my.cnf with the mysql username and password first.
+# FORMAT:
+#
+# [mysqldump]
+# user=<username>
+# password=<password>
+# 
+
 # Database credentials
 user="objured"
 #password=""
@@ -14,11 +22,11 @@ backup_days="180"
 # Create backup directory if it does not already exist
 sudo mkdir -p "$backup_path"
 
-# Set default file permissions
-sudo umask 177
+# Set default file permissions (not for root use)
+# umask 177
 
 # Dump database into SQL file
-sudo mysqldump -p --user=$user --host=$host $db_name > $backup_path/$db_name-$date.sql
+sudo mysqldump --host=$host $db_name > $backup_path/$db_name-$date.sql
 
 # Delete files older than 30 days
 sudo find $backup_path/* -mtime +$backup_days -exec rm {} \;
