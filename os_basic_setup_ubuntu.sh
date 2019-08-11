af5cef7b1ad73ce4a14c8c2ca9683b2d0db307fd
sellitus <sellitus@gmail.com>
2019-08-11 07:36:04 +0000

    Removed sublime-text ppa

--- os_basic_setup_ubuntu.sh
+++ os_basic_setup_ubuntu.sh
@@ -230,13 +230,6 @@ if [[ $guiChoice == "y" ]]; then
 	sudo dpkg -i /tmp/google-chrome-stable_current_amd64.deb
 	rm /tmp/google-chrome-stable_current_amd64.deb
 
-	# Sublime 3
-	wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-	sudo apt-add-repository "deb https://download.sublimetext.com/ apt/stable/"
-	
-	wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-	echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-
 	# Update apt cache
 	sudo apt update
 
