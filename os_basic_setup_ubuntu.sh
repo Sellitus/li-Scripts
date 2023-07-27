3dfbfc648c0e7009e56fa53cfb98dfeb936fc690
Sellitus <sellitus@gmail.com>
2023-07-27 12:05:14 -0500

    Remove changing python version for OS

--- os_basic_setup_ubuntu.sh
+++ os_basic_setup_ubuntu.sh
@@ -181,13 +181,7 @@ if [[ $basicChoice == "y" ]]; then
 	tmux attach-session -t main10 || tmux new-session -s main10
 }' >> ~/.bashrc
 
-
-	# Replace Python 3.10 with 3.11
 	sudo apt-get install python3.11
-
-	sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.10 1
-	sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.11 10
-
 
 	# Setup all the Python 3 Packages
 	sudo apt install -y software-properties-common
