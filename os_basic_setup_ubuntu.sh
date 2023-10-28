cd0f0a9e99166340b9b4401a5b34b0c87206e8a2
SI <sellitus@gmail.com>
2023-10-28 03:01:49 -0500

    Change Docker fix - tested

--- os_basic_setup_ubuntu.sh
+++ os_basic_setup_ubuntu.sh
@@ -334,27 +334,12 @@ if [[ $basicChoice == "y" ]]; then
 	echo ""
 	echo ""
 
- 	# Prevent docker from opening ports publicly
- 	echo "# BEGIN UFW AND DOCKER
-*filter
-:ufw-user-forward - [0:0]
-:DOCKER-USER - [0:0]
--A DOCKER-USER -j RETURN -s 10.0.0.0/8
--A DOCKER-USER -j RETURN -s 172.16.0.0/12
--A DOCKER-USER -j RETURN -s 192.168.0.0/16
-
--A DOCKER-USER -j ufw-user-forward
-
--A DOCKER-USER -j DROP -p tcp -m tcp --tcp-flags FIN,SYN,RST,ACK SYN -d 192.168.0.0/16
--A DOCKER-USER -j DROP -p tcp -m tcp --tcp-flags FIN,SYN,RST,ACK SYN -d 10.0.0.0/8
--A DOCKER-USER -j DROP -p tcp -m tcp --tcp-flags FIN,SYN,RST,ACK SYN -d 172.16.0.0/12
--A DOCKER-USER -j DROP -p udp -m udp --dport 0:32767 -d 192.168.0.0/16
--A DOCKER-USER -j DROP -p udp -m udp --dport 0:32767 -d 10.0.0.0/8
--A DOCKER-USER -j DROP -p udp -m udp --dport 0:32767 -d 172.16.0.0/12
-
--A DOCKER-USER -j RETURN
-COMMIT
-# END UFW AND DOCKER" | sudo tee -a /etc/ufw/after.rules
+ 	# Prevent docker from opening ports publicly, may prevent docker containers from being able to expose ports publicly
+ 	echo '{
+  "iptables": false,
+  "ip6tables": false
+}' | sudo tee -a /etc/docker/daemon.json
+	sudo systemctl restart docker
 
 	# Enable UFW
 	sudo ufw --force enable
