b53872d6d1450543dda5f9e365f4343d04a310d9
SI <sellitus@gmail.com>
2024-04-02 11:35:57 -0500

    Add formatter to vs code

--- os_basic_setup_ubuntu.sh
+++ os_basic_setup_ubuntu.sh
@@ -297,6 +297,7 @@ if [[ $guiChoice == "y" ]]; then
 	code --install-extension ms-azuretools.vscode-docker
 	code --install-extension ms-vscode-remote.remote-wsl
 	code --install-extension ms-vscode-remote.vscode-remote-extensionpack
+ 	code --install-extension esbenp.prettier-vscode
 
 
 
