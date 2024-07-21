117ebeb0d7074916e9b67f2feb3d2f6be6f91a78
sellitus <sellitus>
2024-07-21 00:07:26 -0500

    Rename setup script and update Sublime install

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -286,8 +286,8 @@ if [[ $guiChoice == "y" ]]; then
 	rm /tmp/google-chrome-stable_current_amd64.deb
 	
 	# Install Sublime Text
-	wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-	sudo apt-add-repository "deb https://download.sublimetext.com/ apt/stable/"
+	wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null
+	echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 	
 	# Install Pycharm Community Edition
 	sudo snap install pycharm-community --classic
